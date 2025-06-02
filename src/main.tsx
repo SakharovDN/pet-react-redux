7d541c890c1262a40759da7fb74b6f05bc72f1df
SakharovDN <deafsah@gmail.com>
2025-06-02 23:07:08 +0700

    feat(styles): add main styles, color palette, and reset styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router';
 import { App } from './app/App';
 import store from './app/store';
 
+import './app/styles/main.scss';
+
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <BrowserRouter>
