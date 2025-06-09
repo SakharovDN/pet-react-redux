10056b860a879a2fe5199d1a8e08ada03f08086d
SakharovDN <deafsah@gmail.com>
2025-06-10 02:33:55 +0700

    cicd: add GitHub Pages deployment workflow and update Vite config for base path

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 
 import { App } from './app/App';
 import store from './app/store';
@@ -10,8 +10,8 @@ import './app/styles/main.scss';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pet-react-redux/',
   plugins: [react(), checker({ typescript: true }), tsconfigPaths()],
   server: { open: true },
   css: {
